89c5db2afa805bf5b7dc87c8147d8b32890ca8ba
Felipe Godinho Dal Molin <felipegodinhodalmolin@gmail.com>
2025-06-22 17:32:13 -0300

    Criação do modelo Abastecimento

---
+++ src/modelos/Abastecimento.java
@@ -0,0 +1,81 @@
+package modelos;
+
+import java.sql.Date;
+
+public class Abastecimento {
+	private int idAbastecimento, idImpressora, idMateriaPrima;
+	private java.sql.Date data;
+	private int quantidade;
+	private Impressora impressora = null;
+	private MateriaPrima materiaPrima = null;
+	
+	public Abastecimento() {}
+	
+	public Abastecimento(int idAbastecimento, int idImpressora, int idMateriaPrima, Date data, int quantidade) {
+		super();
+		this.idAbastecimento = idAbastecimento;
+		this.idImpressora = idImpressora;
+		this.idMateriaPrima = idMateriaPrima;
+		this.data = data;
+		this.quantidade = quantidade;
+	}
+
+	public int getIdAbastecimento() {
+		return idAbastecimento;
+	}
+
+	public void setIdAbastecimento(int idAbastecimento) {
+		this.idAbastecimento = idAbastecimento;
+	}
+
+	public int getIdImpressora() {
+		return idImpressora;
+	}
+
+	public void setIdImpressora(int idImpressora) {
+		this.idImpressora = idImpressora;
+	}
+
+	public int getIdMateriaPrima() {
+		return idMateriaPrima;
+	}
+
+	public void setIdMateriaPrima(int idMateriaPrima) {
+		this.idMateriaPrima = idMateriaPrima;
+	}
+
+	public java.sql.Date getData() {
+		return data;
+	}
+
+	public void setData(java.util.Date data) {
+		this.data = new Date(data.getTime());
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Impressora getImpressora() {
+		return impressora;
+	}
+
+	public void setImpressora(Impressora impressora) {
+		this.impressora = impressora;
+	}
+
+	public MateriaPrima getMateriaPrima() {
+		return materiaPrima;
+	}
+
+	public void setMateriaPrima(MateriaPrima materiaPrima) {
+		this.materiaPrima = materiaPrima;
+	}
+	
+	
+	
+}
