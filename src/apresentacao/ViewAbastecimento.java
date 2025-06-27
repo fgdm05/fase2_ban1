de46db0616e2e453edc39ac863871e17c552dc4c
Felipe Godinho Dal Molin <felipegodinhodalmolin@gmail.com>
2025-06-27 20:59:28 -0300

    Faltou salvar viewAbastecimento LOL

--- src/apresentacao/ViewAbastecimento.java
+++ src/apresentacao/ViewAbastecimento.java
@@ -1,5 +1,6 @@
 package apresentacao;
 
+import java.sql.Time;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Scanner;
@@ -15,27 +16,38 @@ public class ViewAbastecimento implements View<Abastecimento> {
 	
 	@Override
 	public Abastecimento criar() {
-		
-		Abastecimento abs = new Abastecimento();
-		System.out.println("Impressora (digite o id):");
-		abs.setIdImpressora(sc.nextInt());
-		sc.nextLine();
-		
-		System.out.println("Materia Prima (digite o id):");
-		abs.setIdMateriaPrima(sc.nextInt());
-		sc.nextLine();
-		
-		System.out.println("Quantidade: ");
-		abs.setQuantidade(sc.nextInt());
-		sc.nextLine();
-		
-		System.out.println("Data (DD/MM/AAAA): ");
-		String data = sc.next();
-		sc.nextLine();
-		SimpleDateFormat conv = new SimpleDateFormat("dd/MM/yyyy");
 		try {
+			Abastecimento abs = new Abastecimento();
+			System.out.println("Impressora (digite o id):");
+			abs.setIdImpressora(sc.nextInt());
+			sc.nextLine();
+			
+			System.out.println("Materia Prima (digite o id):");
+			abs.setIdMateriaPrima(sc.nextInt());
+			sc.nextLine();
+			
+			System.out.println("Quantidade: ");
+			abs.setQuantidade(sc.nextInt());
+			sc.nextLine();
+			
+			System.out.println("Data (DD/MM/AAAA): ");
+			String data = sc.next();
+			sc.nextLine();
+			SimpleDateFormat conv = new SimpleDateFormat("dd/MM/yyyy");
+			
 			java.sql.Date sData = new java.sql.Date(conv.parse(data).getTime());
 			abs.setData(sData);
+			
+			System.out.println("Hora (hh:MM:ss):");
+			String hora = sc.next();
+			sc.nextLine();
+			
+			
+			java.sql.Time a = java.sql.Time.valueOf(hora);
+			System.out.println(a);
+			
+			abs.setTime(a);
+			
 			return abs;
 		} catch (ParseException e) {
 			e.printStackTrace();
